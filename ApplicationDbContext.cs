c2c877ce68b816e765e55b1e66378a92ab85c80e
Marcus Aurelio Senju <thegladus323@gmail.com>
2025-06-04 19:25:53 -0500

    Subiendo el proyecto a github

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskCore.Entidades;
+
+namespace TaskCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet <Tarea> Tareas { get; set; }
+    }
+}
