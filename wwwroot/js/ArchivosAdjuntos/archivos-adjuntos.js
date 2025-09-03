a13ee9ee867b194cdbfcb3f9649210b08a44ce53
Marcus <Thegladus323@gmail.com>
2025-09-03 14:25:36 -0500

    Almacenando los archivos en el backend

--- wwwroot/js/ArchivosAdjuntos/archivos-adjuntos.js
+++ wwwroot/js/ArchivosAdjuntos/archivos-adjuntos.js
@@ -1,5 +1,32 @@
-﻿let inputArchivoTarea = document.getElementById('archivoAtarea');
+﻿const urlArchivos = "/api/archvios";
+
+let inputArchivoTarea = document.getElementById('archivoAtarea');
 
 function manejarClickAgregarArchivoAdjunto() {
     inputArchivoTarea.click();
+}
+
+async function manejarSeleccionArchivoTarea(event) {
+    const archivos = event.target.files;
+    const archivosArreglo = Array.from(archivos);
+
+    const idTarea = tareaEditarViewModel.id;
+    const formData = new FormData();
+
+    for (let i = 0; i < archivosArreglo.length; i++) {
+        formData.append('archivos', archivosArreglo[i]);
+    }
+
+    const respuesta = await
+        fetch(`${urlArchivos}/${idTarea}`, {
+            body: formData,
+            method: 'POST'
+        });
+
+    if (!respuesta.ok) {
+        manejarErrorApi(respuesta);
+        return;
+    }
+    const json = await respuesta.json();
+    console.log(json);
 }
